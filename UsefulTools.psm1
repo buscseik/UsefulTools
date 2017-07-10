a3a21ba1df7d4c51532df58effbbb339f73a1188
Krisztian Buscsei <buscseik@gmail.com>
2017-07-10 16:23:43 +0100

    Organize script to PowerShell module.

---
+++ UsefulTools.psm1
@@ -0,0 +1,20 @@
+﻿#region import script
+. $PSScriptRoot\UsefulTools.ps1
+
+#endregion import script
+
+#region export module member
+export-modulemember -function DateEcho 
+export-modulemember -function Get-UpTimeStatistics
+export-modulemember -function Set-AlwaysOnTop
+export-modulemember -function Set-QuickEdit
+export-modulemember -function Watch
+
+
+
+
+
+
+
+
+#endregion export module member
